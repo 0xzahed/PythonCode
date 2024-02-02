7ca8905489ee50a6c0a423febfef5ea4d77bcf02
zahed01x <zahed01x@gmail.com>
2024-02-03 01:13:15 +0600

    first commit

---
+++ ListLoop.py
@@ -0,0 +1,21 @@
+# using for loop
+
+name = ["Hi", "hello", "Good", "Better","Best"]
+"""
+for x in name:
+    print(x)
+
+# ranage() & len() index print korbe
+    
+for aa in range(len(name)):
+        print(aa)
+
+"""
+
+# While Loop
+
+y = 0
+
+while y < len(name):
+ print(name[y])
+ y = y + 1
