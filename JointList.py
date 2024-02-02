77e3657adc4a17e8ced699216c0af662b74236bb
zahed01x <zahed01x@gmail.com>
2024-02-03 02:23:20 +0600

    first commit

---
+++ JointList.py
@@ -0,0 +1,10 @@
+# duita list k ektay add korar jonno use korte hoy
+
+num1 = [1,2,3,4,5,6,7,8,9]
+num2 = [22,34,56,18]
+num3 = num1 + num2
+
+print(num3)
+# duita list add
+num1.extend(num2)
+print(num1)
---
+++ ListExercise.py
@@ -0,0 +1,14 @@
+# print banana
+fruit = ["apple", "banana","cherry"]
+print(fruit[1])
+
+# replace apple to kiwi
+
+fruit[0] = "kiwi"
+print(fruit[0])
+
+#append method that means 
+ 
+fruit.append("Choclate")
+print(fruit)
+
