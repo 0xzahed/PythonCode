ae71d46a227a221bbe794159f382f68cb4375a7d
zahed01x <zahed01x@gmail.com>
2024-02-04 00:19:30 +0600

    first commit

---
+++ UnpackTuple.py
@@ -0,0 +1,11 @@
+fruit = ("Apple", "Banana", "Cherry","cat","dog")
+"""
+(a,b,c) = fruit # fruit er value ei tinta variable e rakhalm j kyta value thkbe sei kyta variable thkte hbe
+
+print(a)
+print(b)
+print(c)
+"""
+# r ekta variable er maddhome sob gula print korte caile sei ekta variable er age * dilei sob gula print krbe
+(a) = fruit 
+print(a)
