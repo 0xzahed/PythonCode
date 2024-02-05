971a690a8502da8b0d220e7943ae30d06b606342
zahed01x <zahed01x@gmail.com>
2024-02-05 22:41:42 +0600

    first commit

---
+++ UperCaseLowerCase.py
@@ -0,0 +1,9 @@
+ch = input("Enter any character: ")
+
+if 'A' <= ch <= 'Z':
+    print(f"'{ch}' is uppercase alphabet.")
+
+elif 'a' <= ch <= 'z':
+    print(f"'{ch}' is lowercase alphabet.")
+else:
+    print(f"'{ch}' is not an alphabet.")
