af6051c633d3ed66e7b57b3ec30aa50006cd172e
zahed01x <zahed01x@gmail.com>
2024-02-05 21:50:37 +0600

    first commit

--- NameOfWeek.py
+++ NameOfWeek.py
@@ -2,18 +2,18 @@
 week = int(input("Enter week number (1-7): "))
 
 if week == 1:
-    print("Monday")
-elif week == 2:
-    print("Tuesday")
-elif week == 3:
-    print("Wednesday")
-elif week == 4:
-    print("Thursday")
-elif week == 5:
-    print("Friday")
-elif week == 6:
     print("Saturday")
-elif week == 7:
+elif week == 2:
     print("Sunday")
+elif week == 3:
+    print("Monday")
+elif week == 4:
+    print("Tuesday")
+elif week == 5:
+    print("Wednesday")
+elif week == 6:
+    print("Thursday")
+elif week == 7:
+    print("Friday")
 else:
-    print("Invalid Input!")
+    print("Invalid!")
