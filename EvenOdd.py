caaf927e8b75c8ead7645fafbdebbc0f467fd094
zahed01x <zahed01x@gmail.com>
2024-02-05 21:35:57 +0600

    first commit

---
+++ EvenOdd.py
@@ -0,0 +1,10 @@
+
+num = int(input("Enter any number to check even or odd: "))
+
+
+if num % 2 == 0:
+    
+    print("Number is Even.")
+else:
+
+    print("Number is Odd.")
---
+++ UserInput.py
@@ -0,0 +1,23 @@
+a = int(input("Enter Number A : "))
+b = int(input("Enter Number B : "))
+
+Additon = a+b
+print(Additon)
+
+Subtraction = a - b
+print(Subtraction)
+
+Multiplication = a * b
+print(Multiplication)
+
+Division = a / b
+print(Division)
+
+Modulus = a % b
+print(Modulus)
+
+Exponentiation = a ** b
+print(Exponentiation)
+
+Floor_Division = a // b
+print(Floor_Division)
