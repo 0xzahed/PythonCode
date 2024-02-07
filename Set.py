76f28d79d2a1567602ba32f6d3dbf82aedad977b
zahed01x <zahed01x@gmail.com>
2024-02-07 23:39:50 +0600

    next

---
+++ Set.py
@@ -0,0 +1,7 @@
+# set unchangable and sob gula data type dewa jabe
+# ar same variable dewa jabena kaj suhdu unique variable kore
+
+#set = {1,True,"String","String"}
+
+Set = {1,"Hi", "Hello", "Let's Go", "Good Bye"}
+print(Set)
