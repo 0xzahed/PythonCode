7fbf5e4228ce5eec4aceb9967ffcb5c502cb005a
zahed01x <zahed01x@gmail.com>
2024-02-08 00:18:16 +0600

    first

---
+++ Loop.py
@@ -0,0 +1,21 @@
+# for loop use hoy jkn amra list e kotobar itaration hobe seta jani tokn for loop use hoy
+# while loop e kotobar itaration hobe seta condition diye dite hoy
+
+# while loop
+"""
+While = 2
+
+while While < 5: # ekhne j condition thkbe sei koybar print krbe
+    print("While Is Less Than ",While)
+    While = While + 1
+
+    """
+
+# For Loop
+
+fruits = ["a","b","c","d"]
+
+for n in fruits:
+    if n == "c":
+        break
+print(n)
