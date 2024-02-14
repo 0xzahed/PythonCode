ea8fa8a0f112faf58b3fa9885b6ca907897d0f0a
zahed01x <zahed01x@gmail.com>
2024-02-14 23:17:34 +0600

    Function

---
+++ Function.py
@@ -0,0 +1,12 @@
+def Addition(a,b):
+    sum = a + b
+    print(sum)
+Addition(10,20)
+Addition(40,50)
+Addition(90,100)
+Addition(40,90)
+print("Multiplication")
+def Multiplication(a,b):
+    Mult = a * b
+    print(Mult)
+Multiplication(30,50)
