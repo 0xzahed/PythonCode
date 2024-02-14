79ab20fdc53eeadf48f6be8cdfc285b643c93db5
zahed01x <zahed01x@gmail.com>
2024-02-15 00:06:50 +0600

    frist commit

---
+++ Array.py
@@ -0,0 +1,7 @@
+# list er motoni kaj kore but kicu kicu jygay defrence ache and list er mtni sobi kora jay
+
+
+MyArray = ["Hi","Hello","Bye"]
+MyArray[1] = "Good"
+
+print(MyArray)
---
+++ Lambda.py
@@ -0,0 +1,12 @@
+# eta dara anonymous function make kora jay
+
+def Fun(a,b):
+    sum = a + b
+    print(sum)
+Fun(20,30)
+
+
+# lambda keyword eti function er motoni kaj kore easy way te kaj kore function er theke
+# simple function e er ceye lambda function dara ek line ei kaj kora jay
+x = lambda a,b: a + b
+print(x(10,20))
