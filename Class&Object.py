7621aa06cd06709021444116d5f52265347febf9
zahed01x <zahed01x@gmail.com>
2024-02-15 00:33:52 +0600

    first commit

---
+++ Class&Object.py
@@ -0,0 +1,14 @@
+class Name: # class
+    name = ""
+    Number = ""
+
+
+a = Name() # Object Name class ti a object er modde add
+b = Name()
+c = Name()
+a.name = "Abu"
+print(a.name)
+b.name = "Zahed"
+print(b.name)
+c.Number = "01964464744"
+print(c.Number)
