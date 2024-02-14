abc22a4e9492a5dc4c834981a966c0bb61049d84
zahed01x <zahed01x@gmail.com>
2024-02-14 23:49:10 +0600

    first commit

--- Zip.py
+++ Zip.py
@@ -1,4 +1,4 @@
 List1 = ["Zahed","Hamja","Shakib"]
 List2 = ["Google","Microsoft","Amazon"]
 
-print(list (zip(List1,List2)))
+print(list (zip(List1,List2))) # zip fuinction diye duita list combine kore ek sthe kora jay eti tuple type print kore
