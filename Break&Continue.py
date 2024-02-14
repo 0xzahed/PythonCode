e75634707cbcb083d1315d459b0d31bbdb192ffd
zahed01x <zahed01x@gmail.com>
2024-02-14 23:33:13 +0600

    first commit

---
+++ Break&Continue.py
@@ -0,0 +1,13 @@
+NewList = [1,2,3,4,5,6,7,8,9,10] # ekne jodi string value o thake tao index number print krbe
+
+for x in range(len(NewList)): # 0 theke start hobe 0,1,2
+    if x  == 3:
+        break
+    print(x)
+
+
+List = [1,2,3,4,5,6,7,8,9,10]
+for n in range(len(List)):
+    if n == 4:
+        continue # jokhon continue keyword use krbo tokhon ei condition kaj krbena 
+    print(n)
