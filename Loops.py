1114f34c106c3517e6bee62f6590a176ff1f315a
zahed01x <zahed01x@gmail.com>
2024-02-18 11:25:23 +0600

    commit

---
+++ Loops.py
@@ -0,0 +1,47 @@
+"""
+count = 1
+while count <= 5:
+    print("Hello", count)
+    count = count + 1
+print(count)
+"""
+"""
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+
+"""
+"""
+n = int(input())
+i = 1
+while i <= 10:
+
+    print(n*i)
+    i += 1
+"""
+"""
+num = [1,4,9,16,25,36,49,64,81,100]
+
+i = 0
+while i < len(num):
+    print(num[i], end=" ")
+    i += 1
+"""
+"""
+num = (1,4,9,16,25,36,49,64,81,100)
+
+i = 0
+while i < len(num):
+    print(num[i], end=" ")
+    i += 1
+
+In = 0
+x = 25
+while In < len(num):
+    if(num[In] == x):
+        print("Index Number:", In)
+    In += 1
+
+"""
+
