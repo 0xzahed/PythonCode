4107a6cca0f68c5e941e741c7313b4091a0f66fc
zahed01x <zahed01x@gmail.com>
2024-03-05 22:22:30 +0600

    commit

--- ListComprehension.py
+++ ListComprehension.py
@@ -4,3 +4,20 @@ num = [1,2,3,4,5]
 
 double = [i * 2 for i in num]
 print(double)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+   newlist.append(x)
+
+print(newlist)
+
+# Convert upper & lower Case
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x.lower() for x in fruits] # x.upper
+
+print(newlist)
